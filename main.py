381020af8244e7a52342ee260bde2ed805d8ff4c
Aubrey Floyd <aubreyfloyd2@gmail.com>
2023-02-06 14:07:01 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Author: Aubrey Floyd
+# GitHub username: aubreyfloyd2
+# Date: 2/6/2023
+# Description: Recursive function that takes two positive integers as parameters and returns
+#              the product of those two numbers.
+
+def multiply(x, y):
+    """Returns the product of two positive integers"""
+    if y == 1:
+        return x
+    return x + multiply(x, y - 1)
+
+# print(multiply(7, 4))
